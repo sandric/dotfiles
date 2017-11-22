d994800f5b6a3b10793c39104e0727e12ebdc51d
sandric <kyrychenkoanton@gmail.com>
2017-11-22 22:03:23 +0200

    Add m4e.

---
+++ mu4e.el
@@ -0,0 +1,13 @@
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
+(require 'mu4e)
+
+(require 'smtpmail)
+(setq message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials
+      '(("smtp.gmail.com" 587 "kyrychenkoanton@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)
