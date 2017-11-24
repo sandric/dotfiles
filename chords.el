72055aaa6dfc787212d717fb296ea1cb36d7f91d
sandric <kyrychenkoanton@gmail.com>
2017-11-24 17:47:49 +0200

    Add chords.

--- chords.el
+++ chords.el
@@ -1,14 +1,6 @@
 (use-package key-chord
   :ensure t
   :config (progn
-            (setq key-chord-in-macros t)
-
-            (key-chord-define-global "WW" 'sandric/transpose-windows)
-            (key-chord-define-global "EE" 'rectangle-mark-mode)
-            (key-chord-define-global "OO" 'sandric/eval-last-or-region)
-
-            (key-chord-define-global "GG" (lambda ()
-                                            (interactive)
-                                            (print major-mode)
-                                            ))
+            (key-chord-define-global "NN" 'nlinum-mode)
+            (key-chord-define-global "TT" 'sandric/open-current-buffer-in-other-frame)
             (key-chord-mode t)))
