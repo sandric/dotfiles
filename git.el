7661dbc421971950abdc16e0c9f3c7e8925e613d
sandric <kyrychenkoanton@gmail.com>
2017-11-24 17:51:43 +0200

    Add git.

--- git.el
+++ git.el
@@ -1,30 +1,3 @@
-(defun sandric/magit-kill-buffers ()
-  "Kill all magit buffers."
-  (interactive)
-  (mapc (lambda (buffer)
-          (with-current-buffer buffer (magit-blame-quit))
-          (when (s-starts-with? "magit" (format "%s" (buffer-local-value 'major-mode buffer)))
-            (kill-buffer buffer)))
-        (buffer-list)))
-
-(defun sandric/magit-blame-in-right-pane ()
-  "Magit blame in right pane"
-  (interactive)
-  (sandric/open-buffer-in-right-pane (current-buffer))
-  (sandric/eval-in-right-pane '(lambda ()
-                                 (interactive)
-                                 (call-interactively 'magit-blame))))
-
-(defun sandric/git-timemachine-in-right-pane ()
-  "Git-timemachine in right pane"
-  (interactive)
-  (sandric/open-buffer-in-right-pane (current-buffer))
-  (sandric/eval-in-right-pane '(lambda ()
-                                 (interactive)
-                                 (call-interactively 'git-timemachine))))
-
-
-
 (use-package magit
   :ensure t
   :commands (magit-toplevel)
@@ -33,16 +6,18 @@
             (setq ediff-window-setup-function 'ediff-setup-windows-plain)
             (setq ediff-diff-options "-w")
             (setq ediff-split-window-function 'split-window-horizontally)
-
-            (define-key magit-mode-map (kbd "C-H-f") 'sandric/magit-kill-buffers))
+            (setq magit-display-buffer-function
+                  (lambda (buffer)
+                    (display-buffer buffer '(display-buffer-same-window)))))
 
   :bind (:map with-editor-mode-map
-              ("C-H-S-r" . with-editor-finish)
-              ("C-H-S-a" . with-editor-cancel)
+              ("<C-f4> r" . with-editor-finish)
+              ("<C-f4> a" . with-editor-cancel)
 
               :map magit-status-mode-map
               ("p" . magit-push-popup)
               ("M-n" . nil)
+              ("<C-f2> r" . magit-refresh)
 
               :map magit-log-mode-map
               ("M-n" . nil)
@@ -57,19 +32,13 @@
               ("C-g" . magit-blame-quit))
   )
 
-(use-package git-gutter+
+(use-package magithub
   :ensure t
   :config (progn
-            (setq git-gutter+-modified-sign "    ")
-            (setq git-gutter+-added-sign "++++")
-            (setq git-gutter+-deleted-sign "----")
+            (setq ghub-token (shell-command-to-string "git config --get github.oauth-token"))))
 
-            (set-face-background 'git-gutter+-modified "yellow")
-
-            (set-face-background 'git-gutter+-added "green")
-            (set-face-background 'git-gutter+-deleted "red")
-
-            (global-git-gutter+-mode)))
+(use-package github-clone
+  :ensure t)
 
 (use-package git-timemachine
   :ensure t
